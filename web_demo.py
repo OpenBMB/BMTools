564af1502f1428fa19f653ca460c66c7ed98e417
Kunlun Zhu <zhuklun@mail2.sysu.edu.cn>
2023-04-07 13:15:15 +0800

    Update web_demo.py

--- web_demo.py
+++ web_demo.py
@@ -18,6 +18,8 @@ tools_mappings = {
     "wikipedia": "http://127.0.0.1:8079/tools/wikipedia/",
     "office-ppt": "http://127.0.0.1:8079/tools/office-ppt/",
     "bing_search": "http://127.0.0.1:8079/tools/bing_search/",
+    "map": "http://127.0.0.1:8079/tools/map/",
+    "stock": "http://127.0.0.1:8079/tools/stock/"
 }
 
 valid_tools_info = load_valid_tools(tools_mappings)
