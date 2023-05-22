b035324c0777fc721a67db5f139941637004df04
Xuanhe Zhou <zhouxuan19@mails.tsinghua.edu.cn>
2023-05-22 15:01:20 +0800

    database diagnosis (add init)

--- bmtools/tools/__init__.py
+++ bmtools/tools/__init__.py
@@ -13,7 +13,7 @@ from . import tutorial
 from . import file_operation
 from . import meta_analysis
 from . import database
-from . import code_interpreter
+from . import db_diag
 
 from .tool import Tool
 from .registry import register
