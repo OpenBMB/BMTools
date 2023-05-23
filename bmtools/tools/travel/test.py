19f931f3e6b3e11d779759aa30ec14214a890003
Rachum-thu <102339729+Rachum-thu@users.noreply.github.com>
2023-05-23 16:07:14 +0800

    Update test.py

--- bmtools/tools/travel/test.py
+++ bmtools/tools/travel/test.py
@@ -1,4 +1,5 @@
-from bmtools.agent import load_single_tools, STQuestionAnswerer
+from bmtools.agent.singletool import load_single_tools, STQuestionAnswerer
+from datetime import datetime, timedelta
 
 tool_name, tool_url = 'travel',  "http://127.0.0.1:8079/tools/travel/"
 tools_name, tools_config = load_single_tools(tool_name, tool_url)
@@ -8,4 +9,7 @@ qa = STQuestionAnswerer()
 
 agent = qa.load_tools(tools_name, tools_config)
 
-agent("What is the airline information from Beijing to Seattle?")
+month_later = str(datetime.now() + timedelta(days=30)).split()[0]
+agent(f"I will go to Seattle from Beijing on {month_later}. Can you make a recommendation on hotels and flight please?")
+agent("Please help me rent a car near Tsinghua University.")
+agent("Where can I visit on weekend if I am going to University of Washington this summer?")
