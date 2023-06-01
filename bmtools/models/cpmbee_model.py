93c6e3a9ac06de72c16d2a2bea0daa2fd5776b88
Shihao Liang <shihaoliang0828@163.com>
2023-06-01 11:44:55 +0800

    Update cpmbee_model.py

--- bmtools/models/cpmbee_model.py
+++ bmtools/models/cpmbee_model.py
@@ -46,6 +46,6 @@ class CpmBeeLLM(LLM):
         return {"model_name": self.model_name}
     
 if __name__ == "__main__":
-    llm = CpmBeeLLM(config_path="/data/private/qinyujia/webcpm/cpm_live/config/cpm-bee-10b.json", ckpt_path="/data/private/qinyujia/pretrained_models/cpm_10b_webcpm_exp.pt")
+    llm = CpmBeeLLM(config_path="path/to/cpm-bee/config.json", ckpt_path="path/to/cpm-bee/checkpoint/")
     print(llm("You are an task creation AI that uses the result of an execution agent to create new tasks with the following objective: What's the weather in Shanghai today? Should I bring an umbrella?, The last completed task has the result: According to the weather report, it is sunny in Shanghai today and there is no precipitation, so you do not need to bring an umbrella.. This result was based on this task description: Make a todo list about this objective: What's the weather in Shanghai today? Should I bring an umbrella?. These are incomplete tasks: . Based on the result, create new tasks to be completed by the AI system that do not overlap with incomplete tasks. Do not generate repetitive tasks (e.g., tasks that have already been completed). If there is not futher task needed to complete the objective, only return NO TASK. Now return the tasks as an array."))
     
