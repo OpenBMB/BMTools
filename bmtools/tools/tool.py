4abf6b8db7a05db9b116c18e05636c80d00f3b8a
thuqinyj16 <835571620@qq.com>
2023-06-02 11:18:10 +0800

    update tool

--- bmtools/tools/tool.py
+++ bmtools/tools/tool.py
@@ -3,23 +3,26 @@ from typing import Optional
 import copy
 
 class Tool(fastapi.FastAPI):
-    """ Tool is inherited from FastAPI class, thus:
-    1. It can act as a server
-    2. It has get method, you can use Tool.get method to bind a function to an url
-    3. It can be easily mounted to another server
-    4. It has a list of sub-routes, each route is a function
+    r""" Tool is inherited from FastAPI class, thus:
+        - It can act as a server
+        - It has get method, you can use Tool.get method to bind a function to an url
+        - It can be easily mounted to another server
+        - It has a list of sub-routes, each route is a function
 
-    Diagram:
-    Root API server (ToolServer object)
-    │     
-    ├───── "./weather": Tool object
-    │        ├── "./get_weather_today": function_get_weather(location: str) -> str
-    │        ├── "./get_weather_forecast": function_get_weather_forcast(location: str, day_offset: int) -> str
-    │        └── "...more routes"
-    ├───── "./wikidata": Tool object
-    │        ├── "... more routes"
-    └───── "... more routes"
+    Args:
+        - tool_name (str): The name of the tool.
+        - description (str): The description of the tool.
+        - name_for_human (str, optional): The name of the tool for humans. Defaults to None.
+        - name_for_model (str, optional): The name of the tool for models. Defaults to None.
+        - description_for_human (str, optional): The description of the tool for humans. Defaults to None.
+        - description_for_model (str, optional): The description of the tool for models. Defaults to None.
+        - logo_url (str, optional): The URL of the tool's logo. Defaults to None.
+        - author_github (str, optional): The GitHub URL of the author. Defaults to None.
+        - contact_email (str, optional): The contact email for the tool. Defaults to "".
+        - legal_info_url (str, optional): The URL for legal information. Defaults to "".
+        - version (str, optional): The version of the tool. Defaults to "0.1.0".
     """
+
     def __init__(
             self,
             tool_name : str,
@@ -34,6 +37,18 @@ class Tool(fastapi.FastAPI):
             legal_info_url : str = "",
             version : str = "0.1.0",
         ):
+        """
+        Diagram:
+            Root API server (ToolServer object)
+            │     
+            ├───── "./weather": Tool object
+            │        ├── "./get_weather_today": function_get_weather(location: str) -> str
+            │        ├── "./get_weather_forecast": function_get_weather_forcast(location: str, day_offset: int) -> str
+            │        └── "...more routes"
+            ├───── "./wikidata": Tool object
+            │        ├── "... more routes"
+            └───── "... more routes"
+        """
         super().__init__(
             title=tool_name,
             description=description,
